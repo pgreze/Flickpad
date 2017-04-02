8407134f6c59fe7cc5ec5bfaf36e5014e982b3e0
pgreze <pierrick.greze@gmail.com>
2017-04-03 00:19:26 +0900

    Add crashlytics timber logger

--- app/src/main/java/fr/pgreze/flickpad/app/FlickpadApp.java
+++ app/src/main/java/fr/pgreze/flickpad/app/FlickpadApp.java
@@ -4,8 +4,10 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 
+import fr.pgreze.flickpad.common.CrashlyticsTree;
 import fr.pgreze.flickpad.common.di.HasComponent;
 import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
 
 public class FlickpadApp extends Application implements HasComponent<AppComponent> {
 
@@ -20,9 +22,14 @@ public class FlickpadApp extends Application implements HasComponent<AppComponen
     @Override
     public void onCreate() {
         super.onCreate();
+        // Base config
         instance = this;
         BuildVariant.config(this);
+
+        // Configure Crashlytics
         Fabric.with(this, new Crashlytics());
+        Timber.plant(new CrashlyticsTree());
+
         component = createComponent();
     }
 
